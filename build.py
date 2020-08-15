3abc4d92c8fe67edc75cec4909a7c20b1b57fa07
Divan Visagie <me@dvisagie.com>
2020-08-15 21:16:18 +0200

    update rss feed

--- build.py
+++ build.py
@@ -1,28 +1,21 @@
 from podgen import Podcast, Episode, Media
 # Create the Podcast
 p = Podcast(
-   name="The Code Kitchen",
-   description="Every Episode, Software Professional Divan Visagie"
-               "Chats about the latest developments and issues in the software industry with co-host Simon Hornby",
-   website="http://dvisagie.com/",
-   explicit=False,
+    name="The Code Kitchen",
+    description="Every Episode, Software Professional Divan Visagie"
+    "Chats about the latest developments and issues in the software industry with co-host Simon Hornby",
+    website="http://dvisagie.com/",
+    explicit=False,
 )
 # Add some episodes
 p.episodes += [
-   Episode(
-     title="Overcomplicated Design",
-     media=Media("http://dvisagie.com/podcast/episodes/episode_1.mp3", 11932295),
-     summary="In this episode we discuss the phenomenon of overcomplicated software design and how in most cases its unecessary",
-   ),
-#    Episode(
-#       title="Alpaca",
-#       media=Media("http://example.org/files/alpaca.mp3", 15363464),
-#       summary="Thousands of years ago, alpacas were already "
-#               "domesticated and bred to produce the best fibers. "
-#               "Case in point: we have found clothing made from "
-#               "alpaca fiber that is 2000 years old. How is this "
-#               "possible, and what makes it different from llamas?",
-#    ),
+    Episode(
+        title="Overcomplicated Design",
+        media=Media(
+            "http://dvisagie.com/podcast/episodes/episode_1.mp3", 11932295),
+        summary="In this episode we discuss the phenomenon of overcomplicated software "
+        "design and how in most cases its unecessary",
+    )
 ]
 # Generate the RSS feed
 rss = p.rss_str()
